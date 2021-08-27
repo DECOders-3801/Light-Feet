5e31707642120e04982d16a51d1fc40f5da9612e
Keston Huang <s4559712@student.uq.edu.au>
2021-08-27 15:56:38 +0800

    signup button created

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Alert, Button, Text, TextInput, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Header } from 'react-native/Libraries/NewAppScreen';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default class App extends Component {
@@ -12,11 +14,43 @@ export default class App extends Component {
       password: '',
     };
   }
+
   
   onLogin() {
     const { username, password } = this.state;
 
     Alert.alert('Credentials', `${username} + ${password}`);
+  }
+
+  onSignup() {
+    return (
+      <View style={styles.container}> 
+        <Text style={styles.heading}>CO2 Visualiser</Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          color= 'white'
+          placeholder={'Username'}
+          placeholderTextColor='white'
+          style={styles.input}
+        />
+        <TextInput 
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          color= 'white'
+          placeholder={'Password'}
+          placeholderTextColor='white'
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={this.onLogin.bind(this)}
+        />
+      </View>
+    );
   }
 
 
@@ -46,6 +80,12 @@ export default class App extends Component {
           title={'Login'}
           style={styles.input}
           onPress={this.onLogin.bind(this)}
+        />
+
+        <Button
+          title={'Sign Up for an Account'}
+          style={styles.input}
+          onPress={this.onSignup.bind(this)}
         />
       </View>
     );
